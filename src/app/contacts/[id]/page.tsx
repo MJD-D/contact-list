383eb004c7f78741fa4b6a46ced10bbbdfefdb55
MJD-D <matthewdalby1995+techtest@gmail.com>
2024-03-28 00:59:47 +0000

    changed useEffect to useMemo

--- src/app/contacts/[id]/page.tsx
+++ src/app/contacts/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useParams} from 'next/navigation';
 import useContacts from '../../stateHooks/UseContacts';
 import ContactCard from '../../components/ContactCard';
@@ -11,19 +11,16 @@ import { Contact } from '@/app/contact';
 const ContactCardPage = () => {
   const {id} = useParams<{id:string}>();
   const { contacts, editContact } = useContacts();
-  const [contact, setContact] = useState<Contact | null>(null);
  
 
-  useEffect(() => {
-    if (!id) return;
-    const foundContact = contacts.find(c => c.id === id);
-    setContact(foundContact || null);
+  const contact = useMemo(() => {
+    if (!id) return null;
+    return contacts.find(c => c.id === id) ?? null;
   }, [id, contacts]);
 
   const handleEditContact = async (editedContact: Contact) => {
     if (!id) return;
     await editContact(id, editedContact);
-    setContact(editedContact); // Update the local state with edited data
   };
 
   if (!contact) {    
