fbd77cb2e1bbf2e60352ef39bf2af692634f3d47
MJD-D <matthewdalby1995+techtest@gmail.com>
2024-03-28 00:03:35 +0000

    removed unused import

--- src/app/components/ContactList.tsx
+++ src/app/components/ContactList.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import useContacts from '../stateHooks/UseContacts';
